0cf8811cd76af1da0ad3708a828be6e7f910b2e9
nico <yuuuuuuuu.13@gmail.com>
2025-10-16 00:38:27 +0900

    ルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [ContactController::class, 'index'])->name('index'); //お問い合わせフォーム入力ページ
+Route::get('/confirm', [ContactController::class, 'confirm'])->name('confirm'); //お問い合わせフォーム確認ページ
+Route::post('/confirm', [ContactController::class, 'store'])->name('store'); //お問い合わせフォームを登録
+Route::get('/thanks', [ContactController::class, 'thanks'])->name('thanks'); //サンクスページ
+
+Route::get('/login', [ContactController::class, 'login'])->name('login'); //ログインページ
+Route::get('/register', [ContactController::class, 'register'])->name('register');//ユーザ登録ページ
+Route::post('/register', [ContactController::class, 'userStore'])->name('user.store');//ユーザをDBに登録
+
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', [ContactController::class, 'admin'])->name('admin');//管理画面
+    });
+
