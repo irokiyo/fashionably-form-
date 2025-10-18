e36806b54f5c962e83e482753ea48ad64822eaea
nico <yuuuuuuuu.13@gmail.com>
2025-10-18 23:13:02 +0900

    モーダル

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -42,7 +42,8 @@
             <div class="frame">
                 <a href="" class="frame-export">エクスポート</a>
             </div>
-            <div class="pagination">{{ $contacts->links() }}</div>
+            <div class="pagination">{{ $contacts->links('vendor.pagination.bootstrap-4') }}</div>
+
             <table class="admin-table">
                 <tr class="table__row-label">
                     <td class="table__label">お名前 </td>
@@ -53,7 +54,7 @@
                 </tr>
                 @foreach ($contacts as $contact)
                 <tr class="table__row-data">
-                    <td class="table__data">{{ $contact->first_name }} {{ $contact->last_name }}</td>
+                    <td class="table__data">{{ $contact->last_name }} {{ $contact->first_name }}</td>
                     <td class="table__data">
                     @if($contact->gender==1)
                     男性
@@ -65,11 +66,66 @@
                     </td>
                     <td class="table__data">{{ $contact->email }}</td>
                     <td class="table__data">{{ $contact->category->content }}</td>
-                    <td class="table__data"><a href="" class="admin__btn-show">詳細</a></td>
+                    <td class="table__data"><a href="#modal-edit-{{ $contact->id }}" class="admin__btn-show">詳細</a></td>
                 </tr>
-                @endforeach
+                    {{-- モーダル--}}
+                    <div id="modal-edit-{{ $contact->id }}" class="modal" role="dialog" aria-modal="true" aria-labelledby="modal-title-{{ $contact->id }}">
+
+                        <div class="panel">
+                            <div class="content">
+                                <form action="" class="modal__form" method="post" >
+                                @csrf
+                                @method('DELETE')
+                                    <div class="detail__group">
+                                        <label class="label">お名前</label>
+                                        <p class="detail">{{ $contact->last_name }} {{ $contact->first_name }}</p>
+                                    </div>
+                                    <div class="detail__group">
+                                        <label class="label">性別</label>
+                                        <p class="detail">
+                                        @if($contact->gender==1)
+                                        男性
+                                        @elseif($contact->gender==2)
+                                        女性
+                                        @else
+                                        その他
+                                        @endif
+                                        </p>
+                                    </div>
+                                    <div class="detail__group">
+                                        <label class="label">メールアドレス</label>
+                                        <p class="detail">{{ $contact->email }}</p>
+                                    </div>
+                                    <div class="detail__group">
+                                        <label class="label">電話番号</label>
+                                        <p class="detail">{{ $contact->tel1 }}{{ $contact->tel2 }}{{ $contact->tel3 }}</p>
+                                    </div>
+                                    <div class="detail__group">
+                                        <label class="label">住所</label>
+                                        <p class="detail">{{ $contact->address }}</p>
+                                    </div>
+                                    <div class="detail__group">
+                                        <label class="label">建物名</label>
+                                        <p class="detail">{{ $contact->building }}</p>
+                                    </div>
+                                    <div class="detail__group">
+                                        <label class="label">お問い合わせの種類</label>
+                                        <p class="detail">{{ $contact->category->content }}</p>
+                                    </div>
+                                    <div class="detail__group">
+                                        <label class="label">お問い合わせ内容</label>
+                                        <p class="detail">{{ $contact->detail }}</p>
+                                    </div>
+                                    <div class="form__btn">
+                                        <button type="submit" class="btn__delete">削除</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
             </table>
         </form>
+        @endforeach
     </div>
 </div>
 @endsection
